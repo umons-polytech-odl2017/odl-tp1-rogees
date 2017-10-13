01e9b83da6f4623bfc581450e2f9fd034d7a33ee
182869 <rodrigue.fotsing@student.umons.ac.be>
2017-10-13 16:42:32 +0200

    bbbbbbbbb

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -5,12 +5,15 @@ public class Exercise1 {
 		// Ajoutez les champs name et age à la classe Person.
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		return new Person(name, age);
 	}
 
 	public static void main(String[] args) {
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+	int age = Integer.parseInt(args[1]);
+	Person p = createPerson(args[0], age);
+	System.out.print("Nom : " + p.getName() + ", age : " + p.getAge());
 	}
 }
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,19 @@
 package exercise1;
 
 class Person {
+      private String name;
+      private int age;
+
+      public Person (String n, int a){
+      	name = n;
+      	age = a;
+	  }
+      public String getName() { return name; }
+      public int getAge () { return age; }
+
+
+
+
+
 }
+
